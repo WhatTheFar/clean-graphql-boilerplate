7f01208e95134468eb034049f75bd864b18fb1fe
BB-8 <ichannel.jakpat@gmail.com>
2018-12-07 22:20:21 +0700

    refactor(module-alias): add more path

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json'
-    }
-  },
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    "^@root(.*)$": "<rootDir>$1",
-    "^@src(.*)$": "<rootDir>/src$1"
-  }
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.json'
+		}
+	},
+	moduleFileExtensions: ['ts', 'js', 'json'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest'
+	},
+	// testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	testEnvironment: 'node',
+	moduleNameMapper: {
+		'^@root(.*)$': '<rootDir>$1',
+		'^@src(.*)$': '<rootDir>/src$1',
+		'^@module(.*)$': '<rootDir>/src/modules$1',
+		'^@model(.*)$': '<rootDir>/src/models$1'
+	}
 };
--- src/module-alias.ts
+++ src/module-alias.ts
@@ -3,6 +3,9 @@ import { join } from 'path';
 
 moduleAlias.addAliases({
 	'@root': join(__dirname, '..'),
-	'@src': __dirname
+	'@src': __dirname,
+	'@module': join(__dirname, 'modules'),
+	'@model': join(__dirname, 'models')
 });
+
 moduleAlias();
